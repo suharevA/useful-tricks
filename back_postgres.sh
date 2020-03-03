5dd6a2740d1ce567fcfb4f0791cd40a4ab8c6531
alexey.suharev <suharev.skenekt@gmail.com>
2020-03-03 11:45:35 +0300

    Add files via upload

---
+++ back_postgres.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+PGPASSWORD='you pass'
+export PGPASSWORD
+pathB=/var/lib/pgsql/9.6/archive/
+dbUser=add user
+database=database name
+
+find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +30 -delete
+pg_dump -U $dbUser $database | gzip > $pathB/UPKM_DATABASE_$(date "+%Y-%m-%d").sql.gz
+
+unset PGPASSWORD
