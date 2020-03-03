5710714cae4969d3e6cc924c05f94613f2d52491
alexey.suharev <suharev.skenekt@gmail.com>
2020-03-03 11:48:02 +0300

    Update back_postgres.sh

--- back_postgres.sh
+++ back_postgres.sh
@@ -8,6 +8,6 @@ dbUser=add user
 database=database name
 
 find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +30 -delete
-pg_dump -U $dbUser $database | gzip > $pathB/UPKM_DATABASE_$(date "+%Y-%m-%d").sql.gz
+pg_dump -U $dbUser $database | gzip > $pathB/add name database_$(date "+%Y-%m-%d").sql.gz
 
 unset PGPASSWORD
